053c48b7d801c5368befb201a0ba267183ceb985
Akshat Raikhere <raikhereakshat@gmail.com>
2025-03-01 18:39:47 +0530

    Update index.js

--- .devcontainer/01_basics/index.js
+++ .devcontainer/01_basics/index.js
@@ -1,1 +1,10 @@
 console.log("bBU")
+let newtoystype = {     
+    name: "soft toy",  // Added comma here
+    othertoys: ["rc cars","soft toys","robots"],     
+    category: {         
+        rc: ["cars","bike","truck"],         
+        normaltoys: ["snakeladder","chess"]
+    }     
+};     
+console.log(newtoystype);
